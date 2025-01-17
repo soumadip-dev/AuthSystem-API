d2ed2f409e4e512278667caeeeb266a9c84039a2
soumadip-dev <soumadipmajila@gmail.com>
2025-01-17 17:25:14 +0530

    style: create the UI of the EmailVerify.tsx

--- client/src/pages/EmailVerify.tsx
+++ client/src/pages/EmailVerify.tsx
@@ -1,8 +1,37 @@
 import type { FC } from 'react';
+import { Navigate } from 'react-router-dom';
+import { assets } from '../assets/assets';
 const EmailVerify: FC = () => {
   return (
-    <div>
-      <h1>EmailVerify</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => Navigate('/')}
+        src={assets.logo}
+        alt="logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer hover:scale-105 transition-transform"
+      />
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">Email Verify OTP</h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter the 6-digit OTP sent to your email.
+        </p>
+        <div className="flex justify-between mb-8">
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength={1}
+                key={index}
+                required
+                className="w-12 h-12 bg-[#333A5C] text-white text-xl text-center rounded-md"
+              />
+            ))}
+        </div>
+        <button className="w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-600 text-white rounded-full">
+          Verify Email
+        </button>
+      </form>
     </div>
   );
 };
