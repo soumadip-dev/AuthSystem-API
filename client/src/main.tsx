d169a743a8eea26d820c5e445e8928c571282cf6
soumadip-dev <soumadipmajila@gmail.com>
2025-01-23 17:25:14 +0530

    removed react query from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
 import { AppContextProvider } from './context/AppContext';
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
 );
