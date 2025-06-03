5d7cff418d37b2e67f7d17664090ac3bb404aed1
soumadip-dev <soumadipmajila@gmail.com>
2025-06-03 11:00:00 +0530

    completed the ui for login and signup

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -18,15 +18,17 @@ const Login: FC = () => {
           {state === 'signup' ? 'Create Your Account' : 'Login to your account'}
         </p>
         <form>
-          <div className="flex items-center gap-3 mb-4 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
-            <img src={assets.person_icon} alt="person" />
-            <input
-              type="text"
-              placeholder="Full Name"
-              required
-              className="bg-transparent outline-none"
-            />
-          </div>
+          {state === 'signup' && (
+            <div className="flex items-center gap-3 mb-4 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+              <img src={assets.person_icon} alt="person" />
+              <input
+                type="text"
+                placeholder="Full Name"
+                required
+                className="bg-transparent outline-none"
+              />
+            </div>
+          )}
           <div className="flex items-center gap-3 mb-4 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.mail_icon} alt="person" />
             <input
@@ -48,9 +50,21 @@ const Login: FC = () => {
 
           <p className="mb-4 text-indigo-500 cursor-pointer">Forgot Password?</p>
 
-          <button className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900">
+          <button className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium">
             {state === 'signup' ? 'Sign Up' : 'Login'}
           </button>
+
+          {state === 'signup' ? (
+            <p className="mt-4 text-center text-gray-400 text-xs">
+              Already have an account?{' '}
+              <span className="cursor-pointer text-blue-400 underline">Login here</span>
+            </p>
+          ) : (
+            <p className="mt-4 text-center text-gray-400 text-xs">
+              Don't have an account?{' '}
+              <span className="cursor-pointer text-blue-400 underline">Sign Up</span>
+            </p>
+          )}
         </form>
       </div>
     </div>
