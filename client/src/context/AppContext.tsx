e88879ca5c1ea69bab6ed2c628fb1b52d470d90d
soumadip-dev <soumadipmajila@gmail.com>
2025-06-11 11:00:00 +0530

    Add If statement to check if VITE_BACKEND_URL is not defined in environment variables

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -12,6 +12,9 @@ interface AppContextProviderProps {
 
 export const AppContextProvider: FC<AppContextProviderProps> = ({ children }) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  if (!backendUrl) {
+    throw new Error('VITE_BACKEND_URL is not defined in environment variables');
+  }
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState<UserDataType | null>(null);
 
