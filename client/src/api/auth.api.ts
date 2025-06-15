a53db4fc219d0e5d93a90be0d9504498922712b7
soumadip-dev <soumadipmajila@gmail.com>
2025-06-15 11:00:00 +0530

    write Mutation for login user in auth.api.ts

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -11,3 +11,13 @@ export const registerUser = async (credentials: RegisterCredentials): Promise<Re
   console.log(response);
   return response.data;
 };
+
+// Mutation for login user
+export const loginUser = async (credentials: LoginCredentials): Promise<RegisterResponse> => {
+  const response = await axiosInstance.post('/api/v1/users/login', {
+    email: credentials.email,
+    password: credentials.password,
+  });
+  console.log(response);
+  return response.data;
+};
