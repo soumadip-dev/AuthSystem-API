a9a2736b0b7b9238ce6a01a0fcb3cfbc2555151c
soumadip-dev <soumadipmajila@gmail.com>
2025-07-03 11:00:00 +0530

    chore: wrap app with BrowserRouter and QueryClientProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </StrictMode>
+  </BrowserRouter>
 );
