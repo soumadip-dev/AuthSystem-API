77632d4a2edad06c836c083015e02a95804687c3
soumadip-dev <soumadipmajila@gmail.com>
2025-07-07 11:00:00 +0530

    feat: add route for OTP verification controller

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   logoutUser,
   sendVerificationEmail,
+  verifyUser,
 } from '../controller/user.controller.js';
 
 //* Create a new Express router
@@ -14,6 +15,7 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 router.post('/send-verification-email', sendVerificationEmail);
+router.post('/verify-user', verifyUser);
 
 //* Export the router
 export default router;
