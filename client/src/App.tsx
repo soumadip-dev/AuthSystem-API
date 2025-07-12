801cbecd46be8052d15d5f80983b49c8406f15be
soumadip-dev <soumadipmajila@gmail.com>
2025-07-12 11:00:00 +0530

    feat: set up routing in App.tsx with Home, Login, EmailVerify, and ResetPassword pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import EmailVerify from './pages/EmailVerify';
+import ResetPassword from './pages/ResetPassword';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
 };
 export default App;
