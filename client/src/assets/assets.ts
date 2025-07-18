a7d99d06f6f298bf793afeb08053490e27a2086d
soumadip-dev <soumadipmajila@gmail.com>
2025-07-18 11:00:00 +0530

    fix: correct imports in assets.ts

--- client/src/assets/assets.ts
+++ client/src/assets/assets.ts
@@ -1,6 +1,6 @@
 import arrow_icon from './arrow_icon.svg';
 import lock_icon from './lock_icon.svg';
-import logo from './logo.png';
+import logo from './logo.svg';
 import mail_icon from './mail_icon.svg';
 import person_icon from './person_icon.svg';
 import hand_wave from './hand_wave.png';
