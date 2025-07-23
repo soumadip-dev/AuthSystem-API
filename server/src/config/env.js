ae4e7735dbd6f1146087f70dc4532e045f882330
soumadip-dev <soumadipmajila@gmail.com>
2025-07-23 11:00:00 +0530

    chore: add Brevo SMTP configuration to env.js

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -8,4 +8,9 @@ export const ENV = {
   BASE_URL: process.env.BASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
+  BREVO_HOST: process.env.BREVO_HOST,
+  BREVO_PORT: process.env.BREVO_PORT,
+  BREVO_USERNAME: process.env.BREVO_USERNAME,
+  BREVO_PASSWORD: process.env.BREVO_PASSWORD,
+  BREVO_SENDEREMAIL: process.env.BREVO_SENDEREMAIL,
 };
