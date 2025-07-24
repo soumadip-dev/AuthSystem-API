0d66c994be38d3d91ee378beffb966aea951315a
soumadip-dev <soumadipmajila@gmail.com>
2025-07-24 11:00:00 +0530

    Imported Header in Homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import type { FC } from 'react';
 import Navbar from '../components/Navbar';
+import Header from '../components/Header';
 
 const Home: FC = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 };
