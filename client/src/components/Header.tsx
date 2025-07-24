7902012dacbff5bec2d86cf56f1aabe71e91afe2
soumadip-dev <soumadipmajila@gmail.com>
2025-07-24 11:00:00 +0530

    Set up basic structure of Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,14 @@
 import type { FC } from 'react';
+import { assets } from '../assets/assets';
 
 const Header: FC = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img src={assets.header_img} alt="header" className="w-36 h-36 rounded-full mb-6" />
+      <h1>
+        Hey Developer <img src={assets.hand_wave} alt="hand wave" className="w-8 aspect-square" />
+      </h1>
+    </div>
+  );
 };
 export default Header;
