0530153616113c9e9719c8137fe14e84d52025a2
soumadip-dev <soumadipmajila@gmail.com>
2025-07-25 11:00:00 +0530

    Styled the Navber littlebit using tailwind

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import type { FC } from 'react';
 import { assets } from '../assets/assets';
 const Navbar: FC = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="logo" className="w-28 sm:w-32 " />
-      <button>Login</button>
+    <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
+      <img src={assets.logo} alt="logo" className="w-15 sm:w-20 " />
+      <button className="flex items-center gap-2 border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all">
+        Login
+      </button>
     </div>
   );
 };
