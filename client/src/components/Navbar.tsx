3adb3d1e7b4e2414a6d7e878e692972608d95bd1
soumadip-dev <soumadipmajila@gmail.com>
2025-07-25 11:00:00 +0530

    added arrow icon in ligin button

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: FC = () => {
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.logo} alt="logo" className="w-15 sm:w-20 " />
       <button className="flex items-center gap-2 border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all">
-        Login
+        Login <img src={assets.arrow_icon} alt="arrow" />
       </button>
     </div>
   );
