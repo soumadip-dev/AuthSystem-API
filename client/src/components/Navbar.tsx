b5a8903fe5838573ebb7a4e2cd7a4ecaf4abb00d
soumadip-dev <soumadipmajila@gmail.com>
2025-07-28 11:00:00 +0530

    import logo in Navber.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,12 @@
 import type { FC } from 'react';
+import { assets } from '../assets/assets';
 const Navbar: FC = () => {
-  return <div>Navber</div>;
+  return (
+    <div>
+      <img src={assets.logo} alt="logo" />
+    </div>
+  );
 };
 export default Navbar;
+
+// git commit --date="2025-07-28T11:00:00" -m "fix import problem in main.tsx"
