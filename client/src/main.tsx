01189b3c3f3399c95c138c112323d6f1955eff26
soumadip-dev <soumadipmajila@gmail.com>
2025-07-30 11:00:00 +0530

    fix import problem in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
