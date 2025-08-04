4e8be552209ccbf23f4788912bf5a4b884e68933
soumadip-dev <soumadipmajila@gmail.com>
2025-08-04 20:29:11 +0530

    feat: add Mongoose model for User

---
+++ Backend/model/User.model.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Create the user model
+const User = mongoose.model('User', userSchema);
+
+// Export the user model
+export default User;
