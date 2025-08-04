4430f18a4530b275e798330a8446a6f8fa2f19a4
soumadip-dev <soumadipmajila@gmail.com>
2025-08-04 23:12:45 +0530

    fix: correct import bug in mailTemplates.js

--- Backend/utils/mailTemplates.js
+++ Backend/utils/mailTemplates.js
@@ -1,4 +1,4 @@
-import { ENV } from './env';
+import { ENV } from './env.js';
 
 export default function generateMailOptions({ user, token, type }) {
   let subject, actionText, actionUrlPath, message;
