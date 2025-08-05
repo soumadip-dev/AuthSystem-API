cc97006189ab97b5418096d0a0923c15ee847932
soumadip-dev <soumadipmajila@gmail.com>
2025-08-05 12:08:21 +0530

    feat: set up login route and verify successful execution

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 // Import the user controller
-import { registerUser, verifyUser } from '../controller/user.controller.js';
+import { registerUser, verifyUser, login } from '../controller/user.controller.js';
 
 // create a new Express router
 const router = Router();
@@ -9,6 +9,7 @@ const router = Router();
 // Define routes
 router.post('/register', registerUser);
 router.get('/verify/:token', verifyUser);
+router.post('/login', login);
 
 // Export the router
 export default router;
