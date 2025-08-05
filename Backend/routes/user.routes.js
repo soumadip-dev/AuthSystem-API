805286249fc008d541407aefd56e85b629763f2f
soumadip-dev <soumadipmajila@gmail.com>
2025-08-05 14:38:46 +0530

    feat: set up logout route and verify successful execution

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { isLoggedIn } from '../middleware/auth.middleware.js';
 // Import the user controller
-import { registerUser, verifyUser, login, getMe } from '../controller/user.controller.js';
+import { registerUser, verifyUser, login, getMe, logout } from '../controller/user.controller.js';
 
 // create a new Express router
 const router = Router();
@@ -11,6 +11,7 @@ router.post('/register', registerUser);
 router.get('/verify/:token', verifyUser);
 router.post('/login', login);
 router.get('/me', isLoggedIn, getMe);
+router.get('/logout', logout);
 
 // Export the router
 export default router;
