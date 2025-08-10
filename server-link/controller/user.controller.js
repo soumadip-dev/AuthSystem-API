afce57b7400ae55701602fa6db0873c738fcc295
soumadip-dev <soumadipmajila@gmail.com>
2025-08-10 13:15:30 +0530

    refactor: rename old_backend folder to server-link

---
+++ server-link/controller/user.controller.js
@@ -0,0 +1,298 @@
+import User from '../model/User.model.js';
+import crypto from 'crypto';
+import nodemailer from 'nodemailer';
+import { ENV } from '../utils/env.js';
+import generateMailOptions from '../utils/mailTemplates.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { isValidEmail, isStrongPassword } from '../utils/validation.js';
+
+// Controller for registering a user
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  // Check if all fields are provided
+  if (!name || !email || !password)
+    return res.status(400).json({ message: 'All fields are required', success: false });
+
+  // Check if email is valid
+  if (!isValidEmail(email))
+    return res.status(400).json({ message: 'Email is not valid', success: false });
+
+  // Check if password is strong enough
+  if (!isStrongPassword(password))
+    return res.status(400).json({ message: 'Password is not strong enough', success: false });
+
+  try {
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: 'User already exists', success: false });
+
+    // If no User found create an user
+    const newUser = await User.create({ name, email, password });
+
+    if (!newUser) return res.status(500).json({ message: 'User not registered', success: false });
+
+    const token = crypto.randomBytes(32).toString('hex');
+
+    // Store the token in the user
+    newUser.verificationToken = token;
+
+    // Save the user
+    await newUser.save();
+
+    // Configure email transport
+    const transporter = nodemailer.createTransport({
+      host: ENV.MAILTRAP_HOST,
+      port: ENV.MAILTRAP_PORT,
+      auth: {
+        user: ENV.MAILTRAP_USERNAME,
+        pass: ENV.MAILTRAP_PASSWORD,
+      },
+    });
+
+    // Send email
+    const mailOptions = generateMailOptions({
+      user: newUser,
+      token,
+      type: 'verify',
+      companyName: 'Auth System',
+    });
+
+    try {
+      await transporter.sendMail(mailOptions);
+    } catch (emailError) {
+      console.error('Email sending failed:', emailError);
+      return res.status(500).json({ message: 'Email sending failed', success: false });
+    }
+
+    res.status(201).json({ message: 'User created successfully', success: true });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Something went wrong', success: false });
+  }
+};
+
+// Controller for user verification
+const verifyUser = async (req, res) => {
+  // Get verification token from URL parameters
+  const { token } = req.params;
+  console.log(token);
+
+  // Check if token is provided
+  if (!token)
+    return res.status(400).json({ message: 'Verification token is required', success: false });
+
+  try {
+    // Find user based on verification token
+    const user = await User.findOne({ verificationToken: token });
+
+    // Check if user found or not
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+
+    // Update user verification status and remove verification token
+    user.isVerified = true;
+    user.verificationToken = undefined;
+
+    // Save user
+    await user.save();
+
+    // Send success response
+    res.status(200).json({ message: 'User verified successfully', success: true });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Something went wrong', success: false });
+  }
+};
+
+// Controller for user login
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if email and password are provided
+  if (!email || !password)
+    return res.status(400).json({ message: 'Email and password are required', success: false });
+
+  try {
+    // Find user based on email
+    const user = await User.findOne({ email });
+
+    // Check if user found or not
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+
+    // Check if user is verified or not
+    if (!user.isVerified)
+      return res.status(401).json({ message: 'User is not verified', success: false });
+
+    // Check if password is correct or not
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(401).json({ message: 'Password is incorrect', success: false });
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id, role: user.role }, ENV.JWT_SECRET, { expiresIn: '1d' });
+
+    // Store jwt token in cookie
+    const cookieOptions = {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: ENV.NODE_ENV === 'production',
+      maxAge: 24 * 60 * 60 * 1000,
+    };
+    res.cookie('jwt', token, cookieOptions);
+
+    // Send success response
+    res.status(200).json({
+      message: 'User logged in successfully',
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Something went wrong', success: false });
+  }
+};
+
+// Controller for getting current user
+const getMe = async (req, res) => {
+  try {
+    // Find user based on id without password
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (!user) return res.status(404).json({ message: 'User not found', success: false });
+
+    res.status(200).json({ message: 'User found', success: true, user });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Something went wrong', success: false });
+  }
+};
+
+// Controller for logout
+const logout = async (req, res) => {
+  try {
+    // Clear the cookie
+    res.cookie('jwt', '', {});
+    // return success message
+    res.status(200).json({ message: 'User logged out successfully', success: true });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Something went wrong when logging out', success: false });
+  }
+};
+
+// Controller for forgot password
+const forgotPassword = async (req, res) => {
+  // Get the email from the request body
+  const { email } = req.body;
+
+  // Check if the email is present or not
+  if (!email) return res.status(400).json({ message: 'Email is required', success: false });
+
+  try {
+    // Find the user based on email
+    const user = await User.findOne({ email });
+
+    // Check if the user is present or not
+    if (!user)
+      return res.status(404).json({ message: 'User not found with this email', success: false });
+
+    // Generate a random token for password reset
+    const token = crypto.randomBytes(32).toString('hex');
+
+    // Store the token inside resetPasswordToken
+    user.resetPasswordToken = token;
+
+    // Set resetPasswordTokenExpire
+    user.resetPasswordExpiry = Date.now() + 10 * 60 * 1000;
+
+    // Save the user after updating
+    await user.save();
+
+    // Configure email transport
+    const transporter = nodemailer.createTransport({
+      host: ENV.MAILTRAP_HOST,
+      port: ENV.MAILTRAP_PORT,
+      auth: {
+        user: ENV.MAILTRAP_USERNAME,
+        pass: ENV.MAILTRAP_PASSWORD,
+      },
+    });
+
+    // Send email
+    const mailOptions = generateMailOptions({
+      user,
+      token,
+      type: 'reset',
+      companyName: 'Auth System',
+    });
+    try {
+      await transporter.sendMail(mailOptions);
+    } catch (error) {
+      console.error('Email sending failed:', emailError);
+      return res.status(500).json({ message: 'Email sending failed', success: false });
+    }
+
+    res.status(200).json({ message: 'Password reset email sent successfully', success: true });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Something went wrong', success: false });
+  }
+};
+
+// Controller for reset password
+const resetPassword = async (req, res) => {
+  // Get reset token from URL parameters
+  const { token } = req.params;
+  const { password } = req.body;
+
+  // Check if token is present or not
+  if (!token) return res.status(400).json({ message: 'Reset token is required', success: false });
+
+  try {
+    // Find user based on reset token
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpiry: { $gt: Date.now() },
+    });
+
+    // Check if user is present or not
+    if (!user)
+      return res
+        .status(404)
+        .json({ message: 'User not found with this reset token', success: false });
+
+    // Check if password is strong enough
+    if (!isStrongPassword(password))
+      return res.status(400).json({ message: 'Password is not strong enough', success: false });
+
+    // Check if password is same as old password
+    const issamePassword = await bcrypt.compare(password, user.password);
+    if (issamePassword)
+      return res.status(401).json({ message: 'Password is same as old password', success: false });
+
+    // Update the user by setting the password(hashing in pre hook) and remove reset token
+    user.password = password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiry = undefined;
+
+    // Save the user after updating
+    await user.save();
+
+    // Send success response
+    res.status(200).json({ message: 'Password reset successfully', success: true });
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(500)
+      .json({ message: 'Something went wrong when resetting password', success: false });
+  }
+};
+
+// Export controllers
+export { registerUser, verifyUser, login, getMe, logout, forgotPassword, resetPassword };
---
+++ server-link/utils/env.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const ENV = {
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  BASE_URL: process.env.BASE_URL,
+  MAILTRAP_HOST: process.env.MAILTRAP_HOST,
+  MAILTRAP_PORT: process.env.MAILTRAP_PORT,
+  MAILTRAP_USERNAME: process.env.MAILTRAP_USERNAME,
+  MAILTRAP_PASSWORD: process.env.MAILTRAP_PASSWORD,
+  MAILTRAP_SENDEREMAIL: process.env.MAILTRAP_SENDEREMAIL,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
