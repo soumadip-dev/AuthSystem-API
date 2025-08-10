b8f487a17d48daaa24f3f25f7e19a486a6c43ebb
soumadip-dev <soumadipmajila@gmail.com>
2025-08-10 16:30:34 +0530

    feat: register user route in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import auth_routes from './routes/user.routes.js';
 
 const app = express();
 
@@ -16,5 +17,8 @@ app.use(cookieParser());
 
 //* Root Route
 app.get('/', (req, res) => res.send('<h1>Hello from authentication backend</h1>'));
+
+//* Routes
+app.use('/api/v1/users', auth_routes);
 
 export default app;
