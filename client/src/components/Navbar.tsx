c2a065ea87f360a84b44c9b68566a01aea161a6f
soumadip-dev <soumadipmajila@gmail.com>
2025-08-11 23:23:10 +0530

    fix: resolve class-related issues in Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import type { FC } from 'react';
 import { assets } from '../assets/assets';
+
 const Navbar: FC = () => {
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
-      <img src={assets.logo} alt="logo" className="w-15 sm:w-20 " />
+      <img src={assets.logo} alt="logo" className="w-[60px] sm:w-20" />
       <button className="flex items-center gap-2 border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all">
         Login <img src={assets.arrow_icon} alt="arrow" />
       </button>
     </div>
   );
 };
+
 export default Navbar;
