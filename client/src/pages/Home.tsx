44a7ec47d1caf2fbb52ffb7a125f82ecbafb2d6d
soumadip-dev <soumadipmajila@gmail.com>
2025-08-12 05:58:46 +0530

    style: improve UI in Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 
 const Home: FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')]">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')] bg-cover bg-center bg-no-repeat">
       <Navbar />
       <Header />
     </div>
