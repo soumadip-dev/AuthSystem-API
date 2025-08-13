c33a1d5af67bfe4c2883aab96f7ac674e7955670
soumadip-dev <soumadipmajila@gmail.com>
2025-08-13 13:06:50 +0530

    feat: display user name from context in header greeting

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,13 @@
-import type { FC } from 'react';
+import { useContext, type FC } from 'react';
 import { assets } from '../assets/assets';
+import { AppContext } from '../context/AppContext';
 
 const Header: FC = () => {
+  const context = useContext(AppContext);
+  if (!context) throw new Error('Login must be used within an AppContextProvider');
+  const { userData, isLoggedIn } = context;
+  console.log(userData);
+
   return (
     <div className="flex flex-col items-center mt-20 px-4 text-center text-gray-800 animate-fadeIn">
       <img
@@ -10,7 +16,7 @@ const Header: FC = () => {
         className="w-36 h-36 rounded-full mb-6 border-4 border-white/50 shadow-lg hover:scale-105 transition-transform duration-300"
       />
       <h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
-        Hey Developer{' '}
+        Hello, {isLoggedIn ? userData?.name : 'Guest'}
         <img src={assets.hand_wave} alt="hand wave" className="w-8 aspect-square animate-wiggle" />
       </h1>
       <h2 className="text-3xl sm:text-5xl font-semibold mb-4 bg-gradient-to-r from-gray-700 to-gray-900 bg-clip-text text-transparent">
